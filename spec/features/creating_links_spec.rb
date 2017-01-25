d7631dcc5c5c3679b142452138bb77a78eb8d7b0
Irene Canuti <irene.canuti@gmail.com>
2017-01-25 15:41:05 +0000

    Add DatabaseCleaner

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,10 +1,19 @@
+# require 'database_cleaner'
+
+# DatabaseCleaner.strategy = :transaction
+#
+# DatabaseCleaner.cleaning do
+
 feature 'add a link' do
   scenario 'add the site address and title to my bookmark manager' do
+    # DatabaseCleaner.strategy = :transaction
+    # DatabaseCleaner.cleaning do
     visit '/links'
     click_button('Add Link')
     fill_in(:title, with: 'Facebook')
     fill_in(:url, with: 'www.facebook.com')
     click_button('Save')
     expect(page).to have_text('Facebook')
-  end
+    end
+  # end
 end
--- spec/features/feature_tests_spec.rb
+++ spec/features/feature_tests_spec.rb
@@ -1,13 +1,19 @@
+# require 'database_cleaner'
+
 # As a time-pressed user
 # So that I can quickly go to web sites I regularly visit
 # I would like to see a list of links on the homepage
 
-feature '1. see a list of links on the homepage' do
-  scenario 'list links' do
-    Link.create(title: "google", url: "www.google.com")
-    visit '/links'
-    within('ul#links') do
-      expect(page).to have_content("google")
+
+  feature '1. see a list of links on the homepage' do
+    scenario 'list links' do
+      # DatabaseCleaner.strategy = :transaction
+      # DatabaseCleaner.cleaning do
+        Link.create(title: "google", url: "www.google.com")
+        visit '/links'
+        within('ul#links') do
+          expect(page).to have_content("google")
+        end
+      # end
     end
   end
-end
