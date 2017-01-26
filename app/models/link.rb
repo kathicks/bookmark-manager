2704b8f220dd5d19f304c943e92821238900129b
Irene Canuti <irene.canuti@gmail.com>
2017-01-26 10:56:26 +0000

    Configure DataMapper to work with heroku

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,6 +11,6 @@ class Link
   property :url,   String
 end
 
-DataMapper.setup(:default, "postgres://localhost/book_manager_#{ENV["RACK_ENV"]}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/book_manager_#{ENV["RACK_ENV"]}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
